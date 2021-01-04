762970e5ca251533a43def96f892cf97672dd875
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2021-01-04 20:25:47 +0900

    プロジェクト ファイルを追加します。

---
+++ Obenkyou/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obenkyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("obenkyou");
+        }
+    }
+}
